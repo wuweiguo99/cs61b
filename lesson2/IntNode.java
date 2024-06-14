03fca4d78f610f0c07d34676f4bc35d2753ca4cf
wuweiguo99 <wuweiguo2005@163.com>
2024-06-14 17:59:52 +0800

    lesson2 inital add

---
+++ lesson2/IntNode.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class IntNode {
+    public int item;
+    public IntNode Node;
+    public  IntNode(int i,IntNode N)
+    {
+        item = i;
+        Node = N;
+    }
+    
+}
