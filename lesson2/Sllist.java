407afd914efe1242d91f529fdb5ca3d9e99b59a0
wuweiguo99 <wuweiguo2005@163.com>
2024-06-21 00:08:38 +0800

    lesson2 commit

--- lesson2/Sllist.java
+++ lesson2/Sllist.java
@@ -15,12 +15,35 @@ public class Sllist {
     {
         return first.item;
     }
+    public void printlist()
+    {
+        IntNode p;
+        
+        p = this.first;
+        for(int i = 0;p!= null;p = p.Next,i++)
+        {
+            System.out.println("list"+i+" item :"+p.item);
+        }
+    }
+    public void addlast(int x)
+    {
+        IntNode p;
+        p = first;
+        while(p.Next !=null)
+        {
+            p = p.Next;
+        }
+        p.Next = new IntNode(x, null);
+    }
     public static void main(String[] args)
     {
         Sllist  L = new Sllist(10);
         L.addsllist(15);
         L.addsllist(5);
         int r = L.getsllist();
+        L.printlist();
+        L.addlast(20);
+        L.printlist();
         System.out.println("L.item:"+r);
 
     }
